76cc431d0fff724eb19ed5eb869dfbe5cab9635c
Alyona-slukinova <slukinova.alyona@gmail.com>
2026-09-23 22:21:12 +0300

    refactor enemies into abstract base class

--- src/objects/enemy.cpp
+++ src/objects/enemy.cpp
@@ -5,30 +5,14 @@
 using biv::Enemy;
 
 Enemy::Enemy(const Coord& top_left, const int width, const int height) 
-	: RectMapMovableAdapter(top_left, width, height) {
+	: AbstractEnemy(top_left, width, height) {
 	vspeed = 0;
 	hspeed = 0.2;
-}
-
-biv::Rect Enemy::get_rect() const noexcept {
-	return {top_left, width, height};
-}
-
-biv::Speed Enemy::get_speed() const noexcept {
-	return {vspeed, hspeed};
 }
 
 void Enemy::process_horizontal_static_collision(Rect* obj) noexcept {
 	hspeed = -hspeed;
 	move_horizontally();
-}
-
-void Enemy::process_mario_collision(Collisionable* mario) noexcept {
-	if (mario->get_speed().v > 0 && mario->get_speed().v != V_ACCELERATION) {
-		kill();
-	} else {
-		mario->kill();
-	}
 }
 
 void Enemy::process_vertical_static_collision(Rect* obj) noexcept {
